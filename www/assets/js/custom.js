3c69c99d2e41bf9dcd9a7454db0e1c27a521b8b7
Selim Doyranlı <selim@radkod.com>
2019-02-15 17:28:41 +0300

    :ok_hand: improved public functions

--- www/assets/js/custom.js
+++ www/assets/js/custom.js
@@ -1,49 +1,141 @@
-$(document).ready(function(e) {
-    //tooltip
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip();
+// Tooltip
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Popover
+$(function() {
+    $("[data-toggle=popover]").popover()
+});
+
+// Wow effected web elements
+$(window).on('load', function(e) {
+    new WOW().init();
+});
+
+// Lazy load
+jQuery(function($) {
+    $("[data-src],[data-bg],.lazy").lazyLoadXT({
+        // options optional
     });
+});
 
-    //popover yukle
-    $(function() {
-        $("[data-toggle=popover]").popover()
-    });
+// Sticky functions
+let footerBottomAdjust = $("footer").height() + 50;
 
+$(".sticky-top-0").sticky({
+    topSpacing: 0
+});
 
-    //wow efektli objeler - effect div load/animate
-    $(window).on('load', function(e) {
-        new WOW().init();
-    });
+$(".sticky-top-10").sticky({
+    topSpacing: 10,
+    bottomSpacing: footerBottomAdjust
+});
 
-    //lazy load
-    jQuery(function($) {
-        $(".lazy").lazyLoadXT();
-        $.lazyLoadXT.onload.addClass = 'animated fadeIn';
-    });
+$(".sticky-top-20").sticky({
+    topSpacing: 20,
+    bottomSpacing: footerBottomAdjust
+});
 
-    //trunk8
-    $('.cut-1-line').trunk8({
+$(".sticky-top-30").sticky({
+    topSpacing: 30,
+    bottomSpacing: footerBottomAdjust
+});
+
+$(".sticky-top-40").sticky({
+    topSpacing: 40,
+    bottomSpacing: footerBottomAdjust
+});
+
+$(".sticky-top-50").sticky({
+    topSpacing: 50,
+    bottomSpacing: footerBottomAdjust
+});
+
+$(".sticky-top-60").sticky({
+    topSpacing: 60,
+    bottomSpacing: footerBottomAdjust
+});
+
+$(".sticky-top-70").sticky({
+    topSpacing: 70,
+    bottomSpacing: footerBottomAdjust
+});
+
+$(".sticky-top-80").sticky({
+    topSpacing: 80,
+    bottomSpacing: footerBottomAdjust
+});
+
+$(".sticky-top-90").sticky({
+    topSpacing: 90,
+    bottomSpacing: footerBottomAdjust
+});
+
+$(".sticky-top-100").sticky({
+    topSpacing: 100,
+    bottomSpacing: footerBottomAdjust
+});
+
+// Check target click
+$("html").click(function(event) {
+    if (!$(event.target).is(".ex *")) {
+        //
+    } else {
+        //
+    }
+});
+
+// Dynamic background
+$(".shbg").each(function() {
+    let attr = $(this).attr("data-shbg");
+    if (typeof attr !== typeof undefined && attr !== false) {
+        $(this).css("background-image", "url(" + attr + ")");
+    }
+});
+
+// Trunk8 - cut text line
+function cutline() {
+    $(".cut-1-line").trunk8({
         lines: 1
     });
-    $('.cut-2-line').trunk8({
-        lines: 2,
-        fill: '&hellip;'
+    $(".cut-2-line").trunk8({
+        lines: 2
     });
-    $('.cut-3-line').trunk8({
+    $(".cut-3-line").trunk8({
         lines: 3
     });
-    $('.cut-4-line').trunk8({
+    $(".cut-4-line").trunk8({
         lines: 4
     });
-    $('.cut-5-line').trunk8({
+    $(".cut-5-line").trunk8({
         lines: 5
     });
+}
 
-    //fastclick
-    window.addEventListener('load', function() {
-        new FastClick(document.body);
-    }, false);
-
-
-
+$(window).on('load', function() {
+    cutline();
 });
+
+$(window).on('resize', function() {
+    cutline();
+});
+
+// Object fit pollyfill
+$(function() {
+    objectFitImages();
+});
+
+// Public mobile functions
+$(window).bind('DOMContentLoaded load resize', function() {
+    if ($(window).innerWidth() <= 992) {
+        //
+    } else {
+        //
+    }
+});
+
+// Fastclick
+window.addEventListener('load', function() {
+    new FastClick(document.body);
+}, false);
