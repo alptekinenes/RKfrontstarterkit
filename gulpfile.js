bf1fffb135f24d3c5cc8ca9cea7f5775ccab3ed0
Selim Doyranlı <selim@radkod.com>
2019-03-26 20:03:07 +0300

    :package: added new gulp package(s)

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,12 @@ const wait = require('gulp-wait');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const gutil = require('gulp-util');
+const pugInheritance = require('gulp-pug-inheritance');
 const pug = require('gulp-pug');
+const changed = require('gulp-changed');
+const cached = require('gulp-cached');
+const gulpif = require('gulp-if');
+const filter = require('gulp-filter')
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
@@ -15,29 +20,40 @@ const reload = browserSync.reload;
 gulp.task('pug', () => {
     return gulp.src('dev/pug/**/*.pug')
         .pipe(plumber({
-            errorHandler: function (err) {
+            errorHandler: function(err) {
                 notify.onError({
-                    title: "Gulp error in " + err.plugin,
+                    title: "Gulp pug error in " + err.plugin,
                     message: err.message
                 })(err);
                 gutil.beep();
                 this.emit('end');
             }
         }))
+        .pipe(changed('./www/', { extension: '.html' }))
+        .pipe(gulpif(global.isWatching, cached('pug')))
+        .pipe(pugInheritance({ basedir: 'dev/pug', skip: 'node_modules' }))
+        .pipe(filter(function(file) {
+            return !/\/_/.test(file.path) && !/^_/.test(file.relative);
+        }))
         .pipe(pug({
             pretty: true
         }))
         .pipe(gulp.dest('./www/'))
+        .pipe(reload({ stream: true }))
+});
+
+gulp.task('setWatchPugInheritance', function() {
+    global.isWatching = true;
 });
 
 // Compile sass files to css
-gulp.task('sass', function () {
+gulp.task('sass', () => {
     return gulp.src('dev/scss/*.scss')
         .pipe(wait(1000))
         .pipe(plumber({
-            errorHandler: function (err) {
+            errorHandler: function(err) {
                 notify.onError({
-                    title: "Gulp error in " + err.plugin,
+                    title: "Gulp scss error in " + err.plugin,
                     message: err.message
                 })(err);
                 gutil.beep();
@@ -52,11 +68,11 @@ gulp.task('sass', function () {
             cascade: false
         }))
         .pipe(gulp.dest('www/assets/css/'))
-        .pipe(browserSync.reload({ stream: true }))
+        .pipe(reload({ stream: true }))
 });
 
 // the working directory
-gulp.task('browser-sync', ['sass', 'pug'], function () {
+gulp.task('browser-sync', ['sass', 'pug', 'setWatchPugInheritance'], function() {
     browserSync.init({
         server: {
             baseDir: "./www/"
@@ -65,11 +81,10 @@ gulp.task('browser-sync', ['sass', 'pug'], function () {
 });
 
 // Watch files comiling
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('dev/pug/**/*.pug', ['pug']);
     gulp.watch('dev/scss/**/*.scss', ['sass']);
-    gulp.watch('www/assets/js/*.js').on('change', reload);
-    gulp.watch('www/*.html').on('change', reload);
+    gulp.watch('www/assets/js/**/*.js').on('change', reload);
 });
 
 gulp.task('default', ['watch', 'browser-sync']);
